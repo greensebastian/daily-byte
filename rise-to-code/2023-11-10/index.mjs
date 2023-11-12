1690631346d7fb25dfc5a799d08ba6c112093f05
Sebastian Green <sebastianpetergreen@gmail.com>
2023-11-12 21:39:06 +0100

    QoL

--- rise-to-code/2023-11-10/index.mjs
+++ rise-to-code/2023-11-10/index.mjs
@@ -21,12 +21,14 @@ for(const key of connections.keys()){
     connections.set(key, [...new Set(connections.get(key))].sort((a, b) => a - b));
 }
 
+const getWithPrev = (val, prev) => ({ val, prev });
+
 const minimums = new Map();
 
 const getStringIndex = (a, b) => a <= b ? `${a}-${b}` : `${b}-${a}`;
 
-const exploreNodeBFS = (start, end) => {
-    if (minimums.has(getStringIndex(start, end))) return;
+const exploreNodeBFS = (start, end, force = false) => {
+    if (minimums.has(getStringIndex(start, end)) && !force) return;
     let connectionCount = 0;
     let toExplore = [[start]];
     let seen = new Set();
@@ -60,21 +62,47 @@ for(const start of allIndices){
     if (start % 100 === 0) console.info(`Start: ${start}`);
     for(const end of allIndices){
         //if (end % 100 === 0) console.info(`End: ${end}`);
-        exploreNodeBFS(start, end);
+        exploreNodeBFS(start, end, true);
     }
 }
 
 let longestPair = "";
 let longestCount = 0;
+const pairs = [];
 for (const [pair, count] of minimums.entries()){
+    pairs.push({key: pair, count});
     if (count > longestCount){
         longestPair = pair;
         longestCount = count;
     }
 }
 
+const getComplexity = (key, count) => parseInt(key.split('-')[0]) * parseInt(key.split('-')[1]) * (count - 1);
+
+pairs.sort((a, b) => b.count - a.count);
+const maxPairs = pairs.filter(pair => pair.count === pairs[0].count);
+const maxPairsWithComplexity = maxPairs.map(pair => ({...pair, complexity: getComplexity(pair.key, pair.count)})).sort((a, b) => b.complexity - a.complexity);
+console.table(maxPairsWithComplexity)
+for(const pair of maxPairsWithComplexity){
+    const start = parseInt(pair.key.split('-')[0]);
+    const end = parseInt(pair.key.split('-')[1]);
+    exploreNodeBFS(start, end, true)
+    const recomputedDistance = minimums.get(pair.key);
+    const complexity = getComplexity(pair.key, recomputedDistance);
+    console.log(`${pair.key}, ${recomputedDistance}, ${complexity}`);
+}
+
 console.info(longestPair);
 console.info(longestCount);
-const complexity = parseInt(longestPair.split('-')[0]) * parseInt(longestPair.split('-')[1]) * (longestCount - 1);
+const complexity = getComplexity(longestPair, longestCount);
 console.info(complexity);
 // not 410670
+
+/**
+ * 39-49
+ * 49-186
+ * 49-675
+ * 39-390
+ * 186-390
+ * 390-675
+ */
